0b885229407d0ff40a1ce2f0a09fc21c591d2418
Carlos Andres Alzate <unarteman@gmail.com>
2025-01-23 02:21:45 -0300

    construyedo la pagina de estudiantes: muchos issues

--- src/app/pages/students/students.component.ts
+++ src/app/pages/students/students.component.ts
@@ -1,12 +1,65 @@
 import { Component } from '@angular/core';
+import { Student } from './models/student';
+import { generateRandomID } from '../../shared/utils';
+import { FormBuilder } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-students',
   standalone: false,
-  
+
   templateUrl: './students.component.html',
-  styleUrl: './students.component.scss'
+  styleUrl: './students.component.scss',
 })
 export class StudentsComponent {
+  studentForm: FormGroup;
 
+  displayedColumns: string[] = ['edit', 'id', 'name', 'lastName', 'delete'];
+  students: Student[] = [
+    {
+      id: generateRandomID(6),
+      name: 'Hola',
+      lastName: 'Mundo',
+    },
+    {
+      id: generateRandomID(6),
+      name: 'Chris',
+      lastName: 'redfield',
+    },
+    {
+      id: generateRandomID(6),
+      name: 'Jill',
+      lastName: 'Valentine',
+    },
+  ];
+
+  constructor(private fb: FormBuilder) {
+    this.studentForm = this.fb.group({
+      name: [null, [Validators.required]],
+      lastName: [null, [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    if (this.studentForm.invalid) {
+      this.studentForm.markAllAsTouched();
+    } else {
+      this.students = [
+        ...this.students,
+        {
+          id: generateRandomID(6),
+          ...this.studentForm.value,
+        },
+      ];
+
+      console.log(this.students);
+    }
+  }
+
+  onDelete(id: string) {
+    if (confirm('Esta seguro')) {
+      this.students = this.students.filter((el) => el.id != id);
+    }
+  }
 }
--- src/app/pages/students/students.module.ts
+++ src/app/pages/students/students.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormGroup } from '@angular/forms';
 
 import { StudentsRoutingModule } from './students-routing.module';
 import { StudentsComponent } from './students.component';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  declarations: [
-    StudentsComponent
-  ],
-  imports: [
-    CommonModule,
-    StudentsRoutingModule
-  ]
+  declarations: [StudentsComponent],
+  imports: [CommonModule, StudentsRoutingModule, MatFormFieldModule],
+  exports: [],
 })
-export class StudentsModule { }
+export class StudentsModule {}
