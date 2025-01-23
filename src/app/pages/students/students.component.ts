c1facb31fbbf02b3214909183406c92dd909e521
Carlos Andres Alzate <unarteman@gmail.com>
2025-01-23 20:00:32 -0300

    Agrego metodo para eliminar entradas de la tabla; se agrega el metodo al evento click en el boton delete

--- src/app/pages/students/students.component.ts
+++ src/app/pages/students/students.component.ts
@@ -1,86 +1,6 @@
 import { Component } from '@angular/core';
 import { Student } from './models/student';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: Student[] = [
-  {
-    id: '1',
-    name: 'John',
-    lastName: 'Doe',
-    email: 'john.doe@example.com',
-    phoneNumber: '123-456-7890',
-  },
-  {
-    id: '2',
-    name: 'Jane',
-    lastName: 'Smith',
-    email: 'jane.smith@example.com',
-    phoneNumber: '123-555-7890',
-  },
-  {
-    id: '3',
-    name: 'Alice',
-    lastName: 'Brown',
-    email: 'alice.brown@example.com',
-    phoneNumber: '123-456-8888',
-  },
-  {
-    id: '4',
-    name: 'Bob',
-    lastName: 'Davis',
-    email: 'bob.davis@example.com',
-    phoneNumber: '123-456-1234',
-  },
-  {
-    id: '5',
-    name: 'Charlie',
-    lastName: 'Evans',
-    email: 'charlie.evans@example.com',
-    phoneNumber: '555-456-7890',
-  },
-  {
-    id: '6',
-    name: 'Diana',
-    lastName: 'Garcia',
-    email: 'diana.garcia@example.com',
-    phoneNumber: '123-999-7890',
-  },
-  {
-    id: '7',
-    name: 'Edward',
-    lastName: 'Hill',
-    email: 'edward.hill@example.com',
-    phoneNumber: '456-123-7890',
-  },
-  {
-    id: '8',
-    name: 'Fiona',
-    lastName: 'Johnson',
-    email: 'fiona.johnson@example.com',
-    phoneNumber: '987-456-7890',
-  },
-  {
-    id: '9',
-    name: 'George',
-    lastName: 'King',
-    email: 'george.king@example.com',
-    phoneNumber: '123-456-0000',
-  },
-  {
-    id: '10',
-    name: 'Hannah',
-    lastName: 'Lee',
-    email: 'hannah.lee@example.com',
-    phoneNumber: '123-333-7890',
-  },
-];
-
 @Component({
   selector: 'app-students',
   standalone: false,
@@ -89,6 +9,79 @@ const ELEMENT_DATA: Student[] = [
   styleUrl: './students.component.scss',
 })
 export class StudentsComponent {
+  students: Student[] = [
+    {
+      id: '1',
+      name: 'John',
+      lastName: 'Doe',
+      email: 'john.doe@example.com',
+      phoneNumber: '123-456-7890',
+    },
+    {
+      id: '2',
+      name: 'Jane',
+      lastName: 'Smith',
+      email: 'jane.smith@example.com',
+      phoneNumber: '123-555-7890',
+    },
+    {
+      id: '3',
+      name: 'Alice',
+      lastName: 'Brown',
+      email: 'alice.brown@example.com',
+      phoneNumber: '123-456-8888',
+    },
+    {
+      id: '4',
+      name: 'Bob',
+      lastName: 'Davis',
+      email: 'bob.davis@example.com',
+      phoneNumber: '123-456-1234',
+    },
+    {
+      id: '5',
+      name: 'Charlie',
+      lastName: 'Evans',
+      email: 'charlie.evans@example.com',
+      phoneNumber: '555-456-7890',
+    },
+    {
+      id: '6',
+      name: 'Diana',
+      lastName: 'Garcia',
+      email: 'diana.garcia@example.com',
+      phoneNumber: '123-999-7890',
+    },
+    {
+      id: '7',
+      name: 'Edward',
+      lastName: 'Hill',
+      email: 'edward.hill@example.com',
+      phoneNumber: '456-123-7890',
+    },
+    {
+      id: '8',
+      name: 'Fiona',
+      lastName: 'Johnson',
+      email: 'fiona.johnson@example.com',
+      phoneNumber: '987-456-7890',
+    },
+    {
+      id: '9',
+      name: 'George',
+      lastName: 'King',
+      email: 'george.king@example.com',
+      phoneNumber: '123-456-0000',
+    },
+    {
+      id: '10',
+      name: 'Hannah',
+      lastName: 'Lee',
+      email: 'hannah.lee@example.com',
+      phoneNumber: '123-333-7890',
+    },
+  ];
+
   displayedColumns: string[] = [
     'edit',
     'id',
@@ -98,5 +91,15 @@ export class StudentsComponent {
     'phoneNumber',
     'delete',
   ];
-  dataSource = ELEMENT_DATA;
+
+  // dataSource = this.students;
+
+  // metodos
+  onDelete(id: string) {
+    console.log(id);
+
+    if (confirm('esta seguro')) {
+      this.students = this.students.filter((el) => el.id != id);
+    }
+  }
 }
